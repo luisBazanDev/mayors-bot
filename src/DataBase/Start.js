a4aa18edd3d099e125685f1f4e0b6243a0c38a69
Luis Bazán <luis@bazan.pe>
2022-07-02 10:52:31 -0500

    Decoration for MongoDB startup

--- src/DataBase/Start.js
+++ src/DataBase/Start.js
@@ -1,23 +1,27 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
+const colog = require("colog");
 
 mongoose.connection.once("connected", () => {
-  console.log("MongoDB is connected");
+  colog.success("MongoDB is connected");
   const models = fs.readdirSync("./src/Database/models/", {
     encoding: "utf-8",
   });
   for (const model of models) {
     require(`./models/${model}`);
-    console.log(`Loaded Model: ${model.replace(".js")}`);
+    colog.log(
+      colog.cyan(`[MongoDB] Loaded Model: `) +
+        colog.yellow(model.replace(".js", ""))
+    );
   }
 });
 
 mongoose.connection.on("reconnected", () => {
-  console.log(`[${new Date().toUTCString()}] MongoDB is reconnected`);
+  colog.info(`[${new Date().toUTCString()}] MongoDB is reconnected`);
 });
 
 mongoose.connection.on("disconnected", () => {
-  console.log(`[${new Date().toUTCString()}] MongoDB is disconnected`);
+  colog.error(`[${new Date().toUTCString()}] MongoDB is disconnected`);
 });
 
 mongoose.connect(process.env.MONGO_URI);
