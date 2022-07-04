7e7ac26788530554e94da673eba7293df68aa247
Luis Bazán <luis@bazan.pe>
2022-07-04 14:07:29 -0500

    Guild model base complete

--- src/DataBase/models/Guilds.js
+++ src/DataBase/models/Guilds.js
@@ -3,6 +3,10 @@ const { model, Schema } = require("mongoose");
 const Guilds = new Schema({
   guild_id: Number,
   display_name: String,
+  lang: {
+    type: String,
+    default: "EN",
+  },
 });
 
 module.exports = model("Guilds", Guilds);
