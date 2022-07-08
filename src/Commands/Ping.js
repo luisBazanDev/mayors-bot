5333bfc0a868414d5455b3b8f99a78134aa79ad3
Luis Bazán <luis@bazan.pe>
2022-07-07 23:00:12 -0500

    Add permLevel in the Ping command.

--- src/Commands/Ping.js
+++ src/Commands/Ping.js
@@ -2,10 +2,12 @@ const Discord = require("discord.js");
 module.exports = {
   name: "ping",
   description: "Reply ping of the API",
+  permLevel: 50,
   type: 1,
-  execute: async (interaction) => {
+  execute: async (interaction, client) => {
     const Pinging = new Discord.MessageEmbed();
-    Pinging.setTitle("> __**PONG!**__");
+    Pinging.setTitle("__**PONG!**__");
+    Pinging.setDescription(`> ${client.ws.ping}ms`);
 
     interaction.reply({ embeds: [Pinging] });
   },
