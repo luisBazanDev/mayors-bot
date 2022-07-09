ffd3c0374066076b9de1edc013482cccad5c4380
Luis Bazán <luis@bazan.pe>
2022-07-09 15:27:11 -0500

    add to Guilds model funtion for resolve guild's data

--- src/DataBase/models/Guilds.js
+++ src/DataBase/models/Guilds.js
@@ -9,4 +9,20 @@ const Guilds = new Schema({
   },
 });
 
-module.exports = model("Guilds", Guilds);
+const Model = model("Guilds", Guilds);
+Model.resolveGuild = async (guild_id) => {
+  const fetchData = await Model.findOne({
+    guild_id,
+  });
+  if (fetchData) {
+    fetchData.display_name = guild.name;
+    await fetchData.save();
+    return;
+  }
+  const guildData = new Model({
+    guild_id,
+  });
+  await guildData.save();
+};
+
+module.exports = Model;
