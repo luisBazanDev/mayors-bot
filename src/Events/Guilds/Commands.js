732a47f148b076a23539e8fce8ad1e2df449b3aa
Luis Bazán <luis@bazan.pe>
2022-07-09 19:55:57 -0500

    Refactor: discard command from name

--- src/Events/Guilds/Commands.js
+++ src/Events/Guilds/Commands.js
@@ -11,20 +11,17 @@ module.exports = {
     var guildData = await Guilds.resolveGuild(interaction.guild.id);
     if (guildData === null) return;
     client.slashcmds.forEach(async (search, index) => {
-      if (interaction.commandName === search.name) {
-        interaction.lang = require(`../../../resources/langs/${guildData.lang}.json`);
-        if (client.slashcmds[index].permLevel > userData.permissions) {
-          interaction.reply({
-            embeds: [
-              client.format.errorEmbed(
-                interaction.lang.error["dont-permission"]
-              ),
-            ],
-          });
-          return;
-        }
-        client.slashcmds[index].execute(interaction, client);
+      if (interaction.commandName !== search.name) return;
+      interaction.lang = require(`../../../resources/langs/${guildData.lang}.json`);
+      if (client.slashcmds[index].permLevel > userData.permissions) {
+        interaction.reply({
+          embeds: [
+            client.format.errorEmbed(interaction.lang.error["dont-permission"]),
+          ],
+        });
+        return;
       }
+      client.slashcmds[index].execute(interaction, client);
     });
   },
 };
