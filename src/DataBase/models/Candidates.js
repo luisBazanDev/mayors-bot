669fce79cdc3ba34d9c6fba41fa88a1e6e85135f
Luis Bazán <luis@bazan.pe>
2022-07-09 19:54:12 -0500

    Create model of the Candidates

---
+++ src/DataBase/models/Candidates.js
@@ -0,0 +1,36 @@
+const { model, Schema } = require("mongoose");
+
+const Candidates = new Schema({
+  guild_id: String,
+  user_id: String,
+  proposals: {
+    type: Array,
+    default: [],
+  },
+  votos: {
+    type: Number,
+    default: 0,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Model = model("Candidates", Candidates);
+Model.resolveCandidate = async ({ guild_id, user_id }) => {
+  if (!guild_id || !user_id) return undefined;
+  const fetchData = await Model.findOne({
+    guild_id,
+    user_id,
+  });
+  if (fetchData) return fetchData;
+  const CandidateData = new Model({
+    guild_id,
+    user_id,
+  });
+  await CandidateData.save();
+  return CandidateData;
+};
+
+module.exports = Model;
